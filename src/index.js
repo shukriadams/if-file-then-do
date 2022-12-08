5567067ca7cbb7b843412daabfd5e68e2e6f0cf5
Shukri Adams <shukri.adams@gmail.com>
2022-12-09 00:25:02 +0100

    rolled back pkg for windows build support

--- src/index.js
+++ src/index.js
@@ -1,57 +1,62 @@
 (async()=>{
-    // set shortcut global for easier module imports. Modules are loaded relative to "server" directory
-    global._$ = `${__dirname}/`
+    try {
 
-    const path = require('path'),
-        jsonfile = require('jsonfile'),
-        process = require('process'),
-        minimist = require('minimist'),
-        server = require('./lib/server'),
-        watchers = require('./lib/watchers'),
-        argv = minimist(process.argv.slice(2))
+        // set shortcut global for easier module imports. Modules are loaded relative to "server" directory
+        global._$ = `${__dirname}/`
 
-    if (argv.version || argv.v){
-        const package = jsonfile.readFileSync(path.join( __dirname, '/package.json'))
-        console.log(`if-file-then-do, version ${package.version}`)
-        return process.exit(0)
-    }
+        const path = require('path'),
+            jsonfile = require('jsonfile'),
+            process = require('process'),
+            minimist = require('minimist'),
+            server = require('./lib/server'),
+            watchers = require('./lib/watchers'),
+            argv = minimist(process.argv.slice(2))
 
-    if (argv.autostart){
-        const regedit = require('regedit').promisified
-
-        if (process.platform !== 'win32'){
-            console.error('Install app as a service is supported on Windows only')
+        if (argv.version || argv.v){
+            const package = jsonfile.readFileSync(path.join( __dirname, '/package.json'))
+            console.log(`if-file-then-do, version ${package.version}`)
             return process.exit(0)
         }
 
-        await regedit.putValue({
-            'HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run': {
-                iffilethendo: {
-                    value: 'c:\\test\\app.exe',
-                    type: 'REG_SZ'
-                }
+        if (argv.autostart){
+            const regedit = require('regedit').promisified
+
+            if (process.platform !== 'win32'){
+                console.error('Install app as a service is supported on Windows only')
+                return process.exit(0)
             }
-        })
 
-        console.log('autostart enabled')
-        return process.exit(0)
-    }
+            await regedit.putValue({
+                'HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run': {
+                    iffilethendo: {
+                        value: 'c:\\test\\app.exe',
+                        type: 'REG_SZ'
+                    }
+                }
+            })
 
-    if (argv.noautostart){
-        const regedit = require('regedit').promisified
-
-        if (process.platform === 'win32'){
-            console.error('Install app as a service is supported on Windows only')
-            return process(1)
+            console.log('autostart enabled')
+            return process.exit(0)
         }
 
-        await regedit.deleteValue('HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\\iffilethendo')
-        console.log('autostart disabled')
-        return process.exit(0)
-    }
+        if (argv.noautostart){
+            const regedit = require('regedit').promisified
 
-    watchers.start()
-    await server.start()
+            if (process.platform === 'win32'){
+                console.error('Install app as a service is supported on Windows only')
+                return process(1)
+            }
+
+            await regedit.deleteValue('HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\\iffilethendo')
+            console.log('autostart disabled')
+            return process.exit(0)
+        }
+
+        watchers.start()
+        await server.start()
+    } catch(ex){
+        console.log('unexpected error', ex)
+    }
 
 })()
 
